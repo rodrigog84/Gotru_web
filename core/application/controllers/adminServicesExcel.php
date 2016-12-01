46c8e66dc1f8db1e3a9e26b82d510799c01105fd
Rodrigo González <rodrigog.84@gmail.com>
2016-12-01 17:02:48 -0300

    filtra saldos en ceros en cuentas corrientes (pdf y excel)

--- core/application/controllers/adminServicesExcel.php
+++ core/application/controllers/adminServicesExcel.php
@@ -2442,15 +2442,15 @@ class AdminServicesExcel extends CI_Controller {
             $cuentacontable = $this->input->get('cuentacontable');
 
             $sql_filtro = '';
-            if($rutcliente != ''){
+            if($rutcliente != '' && $rutcliente != 'null'){
               $sql_filtro .= "and c.rut = '" . $rutcliente . "'";
 
             }
 
-            if($nombrecliente != ''){
+            if($nombrecliente != '' && $nombrecliente != 'null'){
               $sql_filtro .= "and c.nombres like '%" . $nombrecliente . "%'";
 
-            }if($cuentacontable != ''){
+            }if($cuentacontable != '' && $cuentacontable != 'null'){
 
               $sql_filtro .= "and cco.id = '" . $cuentacontable . "'";
             }
@@ -2469,8 +2469,9 @@ class AdminServicesExcel extends CI_Controller {
                   inner join detalle_cuenta_corriente dcc on dcc.idctacte = cc.id
                   inner join clientes c on cc.idcliente = c.id
                   inner join cuenta_contable cco on cc.idcuentacontable = cco.id
-                  where cc.saldo > 0 " . $sql_filtro . "
+                  where dcc.saldo > 0 " . $sql_filtro . " and dcc.tipodocumento not in (11,102)
                   order by cco.id, c.id, dcc.id");
+
 
             $datas = $query->result_array();
 
